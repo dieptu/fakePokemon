a540d419f71badec2baf3cf1bc496de32654fb00
tuminhdiep <tuminh2307@gmail.com>
2025-03-06 14:40:29 -0500

    animate the player with proper facing direction

--- code/entities.py
+++ code/entities.py
@@ -6,17 +6,37 @@ class Entity(pygame.sprite.Sprite):
 
         #graphics - animation
         self.frame_index , self.frames = 0, frames
+        self.facing_direction = 'down'
+
+        #sprite setup
         self.image = self.frames['down'][self.frame_index]
         self.rect = self.image.get_frect(center = pos)
+        #movement
+        self.direction = vector()
+        self.speed = 250
 
-
+    def animate(self, dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[self.get_state()][int(self.frame_index %len(self.frames[self.get_state()]))]
+    
+    # def update(self, dt):
+    #     self.animate(dt)
+    def get_state(self):
+        #logic
+        moving = bool(self.direction)
+        if moving:
+            if self.direction.x != 0:
+                self.facing_direction = 'right' if self.direction.x > 0 else 'left'
+            if self.direction.y != 0:
+                self.facing_direction = 'down' if self.direction.y > 0 else 'up'
+        return f"{self.facing_direction}{'' if moving else '_idle'}"
 
 class Player(Entity):
     def __init__(self, pos,frames, groups):
         super().__init__(pos, frames, groups)
         # self.image = pygame.Surface((100,100)) #class Entity takes care of this part
         # self.rect = self.image.get_frect(center = pos)
-        self.direction = vector()
+        #self.direction = vector()
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -33,8 +53,9 @@ class Player(Entity):
         self.direction = input_vector
 
     def move(self, dt):
-        self.rect.center += self.direction * 250 * dt #move to direction with a certain speed ()
+        self.rect.center += self.direction * self.speed * dt #move to direction with a certain speed ()
 
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
